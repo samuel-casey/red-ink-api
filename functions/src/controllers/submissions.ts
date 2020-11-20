4af437751d18d121a4b1506eea71cc720fe3ff23
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-20 15:03:39 -0500

    SHOW routes for both submissions/editors and submissions/writers to find all submissions of a given editor or writer

--- functions/src/controllers/submissions.ts
+++ functions/src/controllers/submissions.ts
@@ -1,54 +1,58 @@
-// import * as express from 'express'
-// const router = express.Router();
+import * as express from 'express'
+const router = express.Router();
+import * as db from '../db/connection'
+import {Submission} from  '../db/models/submission'
 
-// // get all submissions assigned to a specific editor
-// router.get('/submissions/editors/:editor_id', async (req: express.Request, res: express.Response) => {
-//     try {
-//         const editorId: string = req.params.editor_id
-//         const allSubmissionsForEditor = await db.collection('submissions').where('editor_id', '==', editorId).get()
+// INDEX -- get all submissions assigned to a specific editor
+router.get('/editors/:editor_id', async (req: express.Request, res: express.Response) => {
+    try {
+        const editorId: string = req.params.editor_id
+        const allSubmissionsForEditor = await db.collection('submissions').where('editor_id', '==', editorId).get()
 
-//         const submissionData: Submission[] = []
+        const submissionData: Submission[] = []
 
-//         allSubmissionsForEditor.forEach((sub: any) => {
-//             submissionData.push({
-//                 sub_id: sub.id,
-//                 editor_id: sub.data().editor_id, 
-//                 writer_id: sub.data().writer_id,
-//                 url: sub.data().url,
-//                 title: sub.data().title,
-//                 edits_complete: sub.data().edits_complete
-//             })
-//         })
+        allSubmissionsForEditor.forEach((sub: any) => {
+            submissionData.push({
+                submission_id: sub.id,
+                editor_id: sub.data().editor_id, 
+                writer_id: sub.data().writer_id,
+                url: sub.data().url,
+                title: sub.data().title,
+                edits_complete: sub.data().edits_complete
+            })
+        })
 
-//         res.status(200).json(submissionData)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
+        res.status(200).json({status:200, message: "ok", data: submissionData})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({status: 400, message: "error", data: error.message})
+    }
+})
 
-// // get all submissions submitted by a specific writer
-// router.get('/submissions/writers/:writer_id', async (req: express.Request, res: express.Response) => {
-//     try {
-//         const writerId: string = req.params.writer_id
-//         const allSubmissionsForWriter = await db.collection('submissions').where('writer_id', '==', writerId).get()
+// INDEX -- get all submissions submitted by a specific writer
+router.get('/writers/:writer_id', async (req: express.Request, res: express.Response) => {
+    try {
+        const writerId: string = req.params.writer_id
+        const allSubmissionsForWriter = await db.collection('submissions').where('writer_id', '==', writerId).get()
 
-//         const submissionData: Submission[] = []
+        const submissionData: Submission[] = []
 
-//         allSubmissionsForWriter.forEach((sub: any) => {
-//             submissionData.push({
-//                 sub_id: sub.id,
-//                 editor_id: sub.data().editor_id, 
-//                 writer_id: sub.data().writer_id,
-//                 url: sub.data().url,
-//                 title: sub.data().title,
-//                 edits_complete: sub.data().edits_complete
-//             })
-//         })
+        allSubmissionsForWriter.forEach((sub: any) => {
+            submissionData.push({
+                submission_id: sub.id,
+                editor_id: sub.data().editor_id, 
+                writer_id: sub.data().writer_id,
+                url: sub.data().url,
+                title: sub.data().title,
+                edits_complete: sub.data().edits_complete
+            })
+        })
 
-//         res.status(200).json(submissionData)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
+        res.status(200).json({status:200, message: "ok", data: submissionData})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({status: 400, message: "error", data: error.message})
+    }
+})
 
-// export = router
+export = router
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import * as cors from 'cors'
 import * as writersController from './controllers/writers'
 import * as editorsController from './controllers/editors'
+import * as submissionsController from './controllers/submissions'
 
 const app = express()
 app.use(express.json())
@@ -11,7 +12,7 @@ app.use(cors())
 // controllers
 app.use('/writers', writersController)
 app.use('/editors', editorsController)
-// app.use('/submissions', submissionsController)
+app.use('/submissions', submissionsController)
 
 // returns json
 app.get('/', (_req: express.Request, res: express.Response) => {
