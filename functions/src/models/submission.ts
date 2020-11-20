60ecb2eb97ea688ca73d0df02b2dc3323b598eb7
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-20 09:41:39 -0500

    submission model

--- functions/src/models/submission.ts
+++ functions/src/models/submission.ts
@@ -1,9 +1,9 @@
 interface Submission {
     submission_id: string;
-    editor_id: string; 
-    writer_id: string;
-    url: string;
     title: string;
+    url: string;
+    writer_id: string;
+    editor_id: string; 
     edits_complete: boolean;
 }
 
