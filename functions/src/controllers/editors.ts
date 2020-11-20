2e5d2b6e55aafd2d555c428bcd40852f79020b0c
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-11-20 13:48:24 -0500

    INDEX and SHOW for editors

--- functions/src/controllers/editors.ts
+++ functions/src/controllers/editors.ts
@@ -1,39 +1,103 @@
-// import * as express from 'express'
-// const router = express.Router();
+import * as express from 'express'
+const router = express.Router();
+import * as db from '../db/connection'
+import {Editor} from  '../db/models/editor'
 
-// router.get('/editors', async (_req: express.Request, res: express.Response) => {
+// INDEX
+router.get('/', async (_req: express.Request, res: express.Response) => {
+    try {
+        const editorsResponse = await db.collection('editors').get()
+        const editors: Editor[] = []
+        editorsResponse.forEach((editor) => {
+            const editorData = {
+                doc_id: editor.id, 
+                uid: editor.data().uid, 
+                first_name: editor.data().first_name,
+                last_name: editor.data().last_name,
+                email: editor.data().email,
+                profile_img_url: editor.data().profile_img_url,
+                linkedin_url: editor.data().linkedin_url,
+                twitter_url: editor.data().twitter_url,
+                about_me: editor.data().about_me
+            }
+            editors.push(editorData)
+        })
+        res.status(200).json({status: 200, message: "ok", data: editors})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({status: 400, message: "error", data: error.message})
+    }
+})
+
+// // SHOW
+// uses UID, not doc_id
+router.get('/:uid', async (req: express.Request, res: express.Response) => {
+    try {
+        const editorId = req.params.uid
+        const theEditorResponse = await db.collection('editors').where('uid', '==', editorId).get()
+        const theEditor: Editor[] = []
+        theEditorResponse.forEach((editor) => {
+            const editorData = {
+                doc_id: editor.id, 
+                uid: editor.data().uid, 
+                first_name: editor.data().first_name,
+                last_name: editor.data().last_name,
+                email: editor.data().email,
+                profile_img_url: editor.data().profile_img_url,
+                linkedin_url: editor.data().linkedin_url,
+                twitter_url: editor.data().twitter_url,
+                about_me: editor.data().about_me
+            }
+            theEditor.push(editorData)
+        })
+        res.status(200).json({status: 200, message: "ok", data: theEditor})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({status: 400, message: "error", data: error.message})
+    }
+})
+
+
+// CREATE
+router.post('/', async (req: express.Request, res: express.Response) => {
+    try {
+        const newEditor: Editor = req.body
+        await db.collection('editors').add(newEditor)
+        res.status(201).json({status: 201, message: "created", data: newEditor})
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({status: 400, message: "error", data: error.message})
+    }
+})
+
+
+// // UPDATE
+// router.put('/:doc_id', async (req: express.Request, res: express.Response) => {
+//     const newEditorData = req.body
 //     try {
-//         const editorsCollection = await db.collection('editors').get()
-//         const allEditors: Editor[] = []
-//         editorsCollection.forEach((editor) => {
-//             allEditors.push({uid: editor.data().uid, email: editor.data().email})
-//         })
-//         res.status(200).json(allEditors)
+//         const editorDocId = req.params.doc_id
+//         const editorToUpdate = db.doc(`editors/${editorDocId}`)
+//         await editorToUpdate.update(newEditorData)
+//         res.status(200).json({status: 200, message: `updated editor with doc_id ${editorDocId}`, data: newEditorData})
 //     } catch (error) {
-//         console.log(error)
+//         console.log(error);
+//         res.status(400).json({status: 400, message: "error", data: error.message})
 //     }
 // })
 
-// // returns new thing from body -- does not save to db
-// router.post('/editors', async (req: express.Request, res: express.Response) => {
+
+// // DESTROY
+// router.delete('/:doc_id', async (req: express.Request, res: express.Response) => {
 //     try {
-//         const newEditor: Editor = req.body
-//         await db.collection('editors').add(newEditor)
-//         res.status(200).json(newEditor) 
+//         const editorDocId = req.params.doc_id
+//         const editorToDelete = db.doc(`editors/${editorDocId}`)
+//         await editorToDelete.delete()
+//         res.status(200).json({status: 200, message: `deleted editor with doc_id ${editorDocId}`})
 //     } catch (error) {
-//         console.log(error)
+//         console.log(error);
+//         res.status(400).json({status: 400, message: "error", data: error.message})
 //     }
 // })
 
-// // to change an editor's email
-// router.put('/editors', async (req: express.Request, res: express.Response) => {
-//     try {
-//         const newEditor: Editor = req.body
-//         await db.collection('editors')
-//         res.status(200).json(newEditor)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// })
 
-// export = router
+export = router
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,20 +2,15 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors'
 import * as writersController from './controllers/writers'
+import * as editorsController from './controllers/editors'
 
-
-
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-// const db = admin.initializeApp().firestore()
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 // controllers
 app.use('/writers', writersController)
-// app.use('/editors', editorsController)
+app.use('/editors', editorsController)
 // app.use('/submissions', submissionsController)
 
 // returns json
